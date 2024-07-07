d3036311e229455d24a59690c42e9d7cbd981cb8
Albert <ron468896@gmail.com>
2024-07-07 22:49:24 +0000

    V1 complete

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="container mx-auto flex flex-col gap-4 justify-between items-center">
         <div className="text-center  mb-8 lg:mb-0">
           <h2 className="text-3xl font-bold">Contact Me</h2>
-          <p className="mt-2">Email: kabu@example.com</p>
+          <p className="mt-2">Email: kabunartey@gmail.com</p>
           <p>Phone: +233 20 783 6067</p>
         </div>
         <div className="flex gap-4">
