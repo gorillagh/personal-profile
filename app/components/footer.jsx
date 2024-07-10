f07b26ff9f038e8c34d667ab5a48b9d53dacd1dc
Albert <ron468896@gmail.com>
2024-07-10 11:32:27 +0000

    Correct the email address

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="container mx-auto flex flex-col items-center justify-between gap-4">
         <div className="mb-8 text-center   lg:mb-0">
           <h2 className="text-3xl font-bold">Contact Me</h2>
-          <p className="mt-2">Email: kabunartey@gmail.com</p>
+          <p className="mt-2">Email: kabunarteyme@gmail.com</p>
           <p>Phone: +233 20 783 6067</p>
         </div>
         <div className="flex gap-4">
